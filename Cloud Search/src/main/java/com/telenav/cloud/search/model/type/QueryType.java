46b35096970e55077a8c620d33500c3d227fe81a
zfshi <zfshi@telenav.cn>
2015-07-01 16:43:16 +0800

    backup

---
+++ Cloud Search/src/main/java/com/telenav/cloud/search/model/type/QueryType.java
@@ -0,0 +1,50 @@
+package com.telenav.cloud.search.model.type;
+
+/**
+ * Created by zfshi on 6/30/2015.
+ */
+public enum QueryType {
+
+    ID("IDQ"),
+
+    KEYWORDS("TQUERY"),
+
+    LATLON("RQBXY"),
+
+    RECTANGLE("SPQ"),
+
+    CENTER("RQBN");
+
+    private String code;
+
+    QueryType(String code) {
+        this.code = code;
+    }
+
+    public QueryType getQueryTypeByCode(String code) {
+        if (ID.getCode().equals(code)) {
+            return ID;
+        }
+        if (KEYWORDS.getCode().equals(code)) {
+            return KEYWORDS;
+        }
+        if (LATLON.getCode().equals(code)) {
+            return LATLON;
+        }
+        if (RECTANGLE.getCode().equals(code)) {
+            return RECTANGLE;
+        }
+        if (CENTER.getCode().equals(code)) {
+            return CENTER;
+        }
+        return null;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+}
