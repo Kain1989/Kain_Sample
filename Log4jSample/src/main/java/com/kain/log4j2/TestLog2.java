a47435b84851848caf94106dbadfcef8742b0933
Shi, Zengfei <shizengfei@aliyun.com>
2019-04-25 11:28:06 -0700

    Change Log4j configuration to extract main thread log to root and archive it to date folder

---
+++ Log4jSample/src/main/java/com/kain/log4j2/TestLog2.java
@@ -0,0 +1,73 @@
+package com.kain.log4j2;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.UUID;
+
+public class TestLog2 {
+
+    private static final Logger log = LogManager.getLogger(TestLog2.class);
+
+    public static void test1() {
+        for (int i = 0; i < 5; i++) {
+            new Thread(() -> {
+                Thread.currentThread().setName(String.valueOf(UUID.randomUUID()));
+                log.info("info");
+                log.debug("debug");
+                log.error("error");
+
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                log.info("info1");
+                log.debug("debug1");
+                log.error("error1");
+
+                try {
+                    Thread.sleep(600);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                log.info("info2");
+                log.debug("debug2");
+                log.error("error2");
+            }).start();
+        }
+    }
+
+    public static void test2() {
+        for (int i = 0; i < 5; i++) {
+            new Thread(() -> {
+                Thread.currentThread().setName(String.valueOf(UUID.randomUUID()));
+                log.info("info3");
+                log.debug("debug3");
+                log.error("error3");
+
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                log.info("info4");
+                log.debug("debug4");
+                log.error("error4");
+
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                for (int j = 0; j < 10000; j++) {
+                    log.info("info5");
+                    log.debug("debug5===================================================================================");
+                    log.error("error5");
+                }
+            }).start();
+
+        }
+    }
+
+}
